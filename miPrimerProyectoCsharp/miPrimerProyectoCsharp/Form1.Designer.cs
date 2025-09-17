aeee968a862ed01dc2c9c7219af49a4114e64ccf
JEFFERSON STEVEN CASTILLO LEIVA <jeffersonk20castillo@gmail.com>
2025-09-17 13:20:24 -0600

    Creacion de la nueva rama academica para computo 2 POO Y BASES DE DATOS

--- miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.Designer.cs
+++ miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace miPrimerProyectoCsharp
 {
-    partial class Form1
+    partial class Academica
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,99 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblSerie = new System.Windows.Forms.Label();
-            this.txtSerie = new System.Windows.Forms.TextBox();
-            this.btnCalcular = new System.Windows.Forms.Button();
-            this.lblMedia = new System.Windows.Forms.Label();
-            this.lblTipica = new System.Windows.Forms.Label();
-            this.lblArmonica = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // lblSerie
-            // 
-            this.lblSerie.AutoSize = true;
-            this.lblSerie.Font = new System.Drawing.Font("Californian FB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSerie.Location = new System.Drawing.Point(54, 81);
-            this.lblSerie.Name = "lblSerie";
-            this.lblSerie.Size = new System.Drawing.Size(64, 27);
-            this.lblSerie.TabIndex = 0;
-            this.lblSerie.Text = "Serie:";
-            // 
-            // txtSerie
-            // 
-            this.txtSerie.Location = new System.Drawing.Point(124, 86);
-            this.txtSerie.Name = "txtSerie";
-            this.txtSerie.Size = new System.Drawing.Size(281, 22);
-            this.txtSerie.TabIndex = 1;
-            // 
-            // btnCalcular
-            // 
-            this.btnCalcular.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalcular.Location = new System.Drawing.Point(124, 286);
-            this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(281, 61);
-            this.btnCalcular.TabIndex = 2;
-            this.btnCalcular.Text = "Calcular";
-            this.btnCalcular.UseVisualStyleBackColor = true;
-            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
-            // 
-            // lblMedia
-            // 
-            this.lblMedia.AutoSize = true;
-            this.lblMedia.Font = new System.Drawing.Font("Californian FB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMedia.Location = new System.Drawing.Point(54, 128);
-            this.lblMedia.Name = "lblMedia";
-            this.lblMedia.Size = new System.Drawing.Size(77, 27);
-            this.lblMedia.TabIndex = 3;
-            this.lblMedia.Text = "Media:";
-            // 
-            // lblTipica
-            // 
-            this.lblTipica.AutoSize = true;
-            this.lblTipica.Font = new System.Drawing.Font("Californian FB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTipica.Location = new System.Drawing.Point(54, 170);
-            this.lblTipica.Name = "lblTipica";
-            this.lblTipica.Size = new System.Drawing.Size(76, 27);
-            this.lblTipica.TabIndex = 4;
-            this.lblTipica.Text = "Tipica:";
-            // 
-            // lblArmonica
-            // 
-            this.lblArmonica.AutoSize = true;
-            this.lblArmonica.Font = new System.Drawing.Font("Californian FB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblArmonica.Location = new System.Drawing.Point(54, 212);
-            this.lblArmonica.Name = "lblArmonica";
-            this.lblArmonica.Size = new System.Drawing.Size(108, 27);
-            this.lblArmonica.TabIndex = 5;
-            this.lblArmonica.Text = "Armonica:";
-            // 
-            // Form1
+            // Academica
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(459, 412);
-            this.Controls.Add(this.lblArmonica);
-            this.Controls.Add(this.lblTipica);
-            this.Controls.Add(this.lblMedia);
-            this.Controls.Add(this.btnCalcular);
-            this.Controls.Add(this.txtSerie);
-            this.Controls.Add(this.lblSerie);
-            this.Name = "Form1";
-            this.Text = "Estadistica";
-         
+            this.Name = "Academica";
+            this.Text = "administracion de alumnos";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblSerie;
-        private System.Windows.Forms.TextBox txtSerie;
-        private System.Windows.Forms.Button btnCalcular;
-        private System.Windows.Forms.Label lblMedia;
-        private System.Windows.Forms.Label lblTipica;
-        private System.Windows.Forms.Label lblArmonica;
     }
 }
 
--- miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
+++ miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
@@ -10,45 +10,14 @@ using System.Windows.Forms;
 
 namespace miPrimerProyectoCsharp
 {
-    public partial class Form1 : Form
+    public partial class Academica : Form
     {
-        public Form1()
+        public Academica()
         {
             InitializeComponent();
         }
 
-        //Matices 
-        //Funciones
-        //LINQ = Lenguaje de Consulta Integrado
-
-        double media(int[] serie)
-        {
-            return serie.Average();
-        }
-        double tipica(int[] serie, double m)
-        {
-            return Math.Sqrt(serie.Average(n => Math.Pow(n - m, 2)));
-        }
-        double armonica(int[] serie)
-        {
-            int n = serie.Length;
-            return n / serie.Sum(num => 1.0 / num);
-        }
-
-        private void btnCalcular_Click(object sender, EventArgs e)
-        {
-            int[] serie = txtSerie.Text.Split(',').Select(n => int.Parse(n)).ToArray(); //5,8,1,9 => ["5", "8", "1", "9"]
-
-            double mediaAritmetica = media(serie);
-            double desviacionTipica = tipica(serie, mediaAritmetica);
-            double mediaArmonica = armonica(serie);
-
-            lblMedia.Text = "Media: " + mediaAritmetica.ToString();
-            lblTipica.Text = "Tipica: " + desviacionTipica.ToString();
-            lblArmonica.Text = "Armonica: " + mediaArmonica.ToString();
-        }
-
-
+       
     }
 }
 
